f0f3679fd4b88f4dc0c5583033fa43bf77279b2c
Mark Agimba <markaguero16@gmail.com>
2019-04-17 10:33:28 +0300

    api and n.base url

--- app/request.py
+++ app/request.py
@@ -1,3 +1,11 @@
 import json
 import urllib.request
-from .models import News, Sources
+from .models import Sources,News
+
+# retreiving api key
+
+api_key = None
+
+# retrieving news base url
+
+base_url = None
