8554e599a73df093d3af79bb5f226c5e9f62b3e9
Mark Agimba <markaguero16@gmail.com>
2019-04-17 10:34:39 +0300

    def configure_request

--- app/request.py
+++ app/request.py
@@ -2,10 +2,15 @@ import json
 import urllib.request
 from .models import Sources,News
 
-# retreiving api key
+# retrieving api key
 
 api_key = None
 
 # retrieving news base url
 
 base_url = None
+
+def configure_request(app):
+    global api_key, base_url
+    api_key = app.config["NEWS_API_KEY"]
+    base_url = app.config["NEWS_API_BASE_URL"]
